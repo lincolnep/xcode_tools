3f8349e08ec794437c0148cf1f6652abb52377ba
Carl <carl@carlashley.com>
2017-09-20 13:47:15 +1000

    Update for macOS 10.13

--- xcode_tools.py
+++ xcode_tools.py
@@ -2,7 +2,8 @@
 
 # Downloads the Xcode CLI tools from the Apple Software Update catalog
 # Also includes the Xcode CLI SDK and Dev SDK
-# Works for CLI Tools version 8.3
+# Works for CLI Tools version 8.3.x, and 9.0. Just be careful which packages you install, as there are a few now that Xcode 9 is released.
+# If you want to use the 10.13 catalog feed, just comment out line 14, and uncomment line 15.
 
 import os
 import plistlib
@@ -11,6 +12,7 @@ import urllib2
 
 xcode_pkg_names = ['CLTools', 'DevSDK']
 su_catalog = 'https://swscan.apple.com/content/catalogs/others/index-10.12.merged-1.sucatalog'  # NOQA
+# su_catalog = 'https://swscan.apple.com/content/catalogs/others/index-10.13.merged-1.sucatalog'  # NOQA
 output_path = os.path.expanduser('~/Desktop/')
 request = urllib2.Request(su_catalog)
 request = urllib2.urlopen(request)
